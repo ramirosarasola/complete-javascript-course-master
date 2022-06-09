e7bbca432359a42a7e70e609938ce116267173cc
Ramiro Sarasola <ramiro.sarasola@gmail.com>
2022-06-09 16:46:29 -0300

    New Section (Setting Up our vscode and dev environment)

---
+++ 03-Developer-Skills/starter/js/script.js
@@ -0,0 +1,8 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+//prettier => we can brose (https://prettier.io/docs/en/configuration.html) for personalization;
+//snippets
+//manage => settings => open setting (JSON) there we can style our vscode ex: comments;
+
+//live server from extension;
